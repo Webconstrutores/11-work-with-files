ffedf189b2872aa0d18668a7aca24cedf867fff9
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-09 13:38:00 -0300

    instantiate BufferedReader via try-with-resources (159)

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "D:\\cursos\\curso-java\\11-files-FileReader_BufferedReader-02\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+				
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			
 			while(line != null) {
@@ -27,19 +22,6 @@ public class Program {
 		}
 		catch(IOException e) {
 			System.out.println("Eror: " + e.getMessage());
-		}
-		finally {
-			try {
-				if(br != null) {
-					br.close();
-				}
-				if(fr != null) {
-					fr.close();
-				}
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
 		}
 
 	}
