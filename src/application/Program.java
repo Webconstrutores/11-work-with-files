fb8b55a29f6feab7706fb8c3fd198023d01e338d
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-09 14:11:08 -0300

    manupulating files system

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,37 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String[] lines = new String[] {"Good morning!", "Good afternoon!", "Good night!"};
-		String path = "D:\\cursos\\curso-java\\11-files-01\\out.txt";
+		Scanner sc = new Scanner(System.in);
 		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-			for(String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		// Listing folders
+		File path = new File(strPath);
+		File[] folders = path.listFiles(File::isDirectory);
+		
+		System.out.println("Folders: ");
+		for(File folder : folders) {
+			System.out.println(folder);
 		}
-		catch(IOException e) {
-			e.printStackTrace();
+		
+		// Listing files
+		File[] files = path.listFiles(File::isFile);
+		for (File file: files) {
+			System.out.println(file);
 		}
+		
+		// Creating folder 
+		boolean success = new File(strPath + ".\\createdFolder").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		sc.close();
 	}
 
 }
